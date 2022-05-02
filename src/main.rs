032772617b3e2cee7a5fc8c8bb91fca12c2f9e41
Frank Loijenga <fjloijenga@gmail.com>
2022-05-02 12:37:00 +0200

    Fixed the errors, the object system is happy now.
    The object system is not complete , it is just functional.
    Future commits should include further functionality of the varients.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod objects;
 
 fn main() {
-    let mut id_ledger: Vec<u32> = vec![0];
+    let mut world = objects::World::new();
+    let mut player = objects::Object::new(0, objects::ObjectVarient::Empty, &mut world);
+    let camera = objects::Object::new(player.id, objects::ObjectVarient::Camera, &mut world);
+    player.add_child(&camera);
+
+    println!("{:?}\n{:?}", player, camera);
 }
--- src/objects.rs
+++ src/objects.rs
@@ -1,33 +1,52 @@
-pub struct IdLedger {
-    ledger: Vec<>
+pub struct World {
+    pub id: u32,
+    pub children: Vec<u32>,
+    pub id_tracker: u32
 }
 
-impl IdLedger {
-    pub fn get_id(&self, parent: u32) {
-        
+impl World {
+    pub fn new() -> World {
+        World {
+            id: 0,
+            children: Vec::new(),
+            id_tracker: 0
+        }
     }
 }
-
+#[derive(Debug)]
 pub enum ObjectVarient {
     Camera,
     RigidBody,
-    Mesh
+    Mesh,
+    Empty
 }
-
+#[derive(Debug)]
 pub struct Object {
     pub position: [f32; 3], // relative from parent
     pub rotation: [f32; 3], // relative from default (axis) in degrees
-    pub id: u32, // fro keeping track of the child-parent structure
-    pub varient: ObjectVarient
+    pub id: u32, // for keeping track of the child-parent structure
+    pub varient: ObjectVarient,
+    pub parent_id: u32,
+    pub children_ids: Vec<u32>
 }
 
 impl Object {
-    fn new(parent_id: u32, varient: ObjectVarient, ledger: Vec<u32>) -> Object {
+    pub fn new(parent_id: u32, varient: ObjectVarient, world: &mut World) -> Object {
+        world.id_tracker += 1;
         Object {
             position: [0.0; 3],
             rotation: [0.0; 3],
-            id: 1,
-            varient
+            id: world.id_tracker,
+            varient,
+            parent_id,
+            children_ids: Vec::new()
         }
+    }
+
+    pub fn add_child(&mut self, child: &Object) {   // adds the id of the given child to the parent vector after checking parent id
+        if child.parent_id != self.id {
+            panic!("The child parent id does not match the id of the parent you want to assign to");
+        }
+        self.children_ids.push(child.id);
     }
 }
