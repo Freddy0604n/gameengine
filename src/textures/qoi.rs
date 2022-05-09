b75a6ee11a9b232887d9cefaa1de3b0fb1a990b0
Frank Loijenga <fjloijenga@gmail.com>
2022-05-09 18:16:04 +0200

    Added textures script and qoi encode funbciton

---
+++ src/textures/qoi.rs
@@ -0,0 +1,7 @@
+pub fn encode(image_data: Vec<[u8; 4]>, image_dimensions: [u8; 2]) -> Vec<u8> { // converts an array of RGBA pixels into the qoi format
+    //TODO
+    let previous: Vec<[u8; 4]> = Vec::new();
+    for i in image_data {
+
+    }
+}
